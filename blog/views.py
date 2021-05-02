90ed37f868e8b8abbed983740e17c6e292b7be34
Saqib <18554574+saqe@users.noreply.github.com>
2021-05-02 06:38:03 +0500

    Added sample blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[{
+        'author'    : 'Saqib',
+        'title'     : 'First DemoPost',
+        'content'   : 'That my first dummy post written in django.',
+        'date_posted': '20-08-2021',
+    },{
+        'author'    : 'Saqib',
+        'title'     : 'First DemoPost (Duplicated)',
+        'content'   : 'That my duplicated dummy post written in django.',
+        'date_posted': '15-05-2021',
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html',)
+    context={
+        'posts':posts,
+        'title':'HomePage'
+    }
+    return render(request, 'blog/home.html',context=context)
 
 def about(request):
     return render(request, 'blog/about.html',)
