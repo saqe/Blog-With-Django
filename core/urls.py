4b9ab849c58fd77ca1d05f12f17650a21880cc11
Saqib <18554574+saqe@users.noreply.github.com>
2021-05-02 06:39:00 +0500

    Added about/ route in urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views as blogViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('about/', blogViews.about, name='blog-about')
 ]
