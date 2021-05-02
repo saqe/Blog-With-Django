7fae08b16a76dba3991e46ce5a6675421884af27
Saqib <18554574+saqe@users.noreply.github.com>
2021-05-02 08:49:41 +0500

    Removed SECRETKEYS and added environment values.

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)8vr^l=cu25vm8j2ylt-#5f2d$t09izdo2o=_1dk^1)nw%==8-'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-)8vr^l=cu25vm8j2ylt-#5f2d$t09izdo2o=_1dk^1)nw%==8-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
