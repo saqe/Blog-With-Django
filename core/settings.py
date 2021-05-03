3f4655f6cb7d6b8738367c85a4d341ae96a3b53e
Saqib <18554574+saqe@users.noreply.github.com>
2021-05-03 06:09:55 +0500

    easy loading enviroment variable on local machine

--- core/settings.py
+++ core/settings.py
@@ -15,8 +15,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
-load_dotenv()
+if not os.getenv('DEBUG'):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
