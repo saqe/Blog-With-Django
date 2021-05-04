60ed6c671d262bc28ab29189af57939e82ad2131
Saqib <18554574+saqe@users.noreply.github.com>
2021-05-04 07:51:10 +0500

    User Registeration HTML code & Django logics added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    # There will be two types of HTTP requets at register route. 
+    # GET & POST requests. 
+    # We need to verify and manage both events differently.
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid(): 
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request,
+                f'Account created with username {username}!'
+            )
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        'html/register.html',
+        {'form':form}
+    )
+
+def login(request):
+    return HttpResponse('login page')
+
+def logout(request):
+    return HttpResponse('logout page')
+
+@login_required
+def profile(request):
+    return HttpResponse('profile page')
