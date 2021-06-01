04fef773aeb9b089e954c3a2b3bc44fa9a6cc0ce
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-01 08:29:41 +0500

    📦 Added profile Model for user.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    country = CountryField()
+    city = models.CharField(max_length=25)
+    
+    profile_image = models.ImageField(default='/staticfiles/images/default_profile_image.jpg', upload_to='profile_pic')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
