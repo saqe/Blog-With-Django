3a337f4eefb349d2eb00726e8407543c4f4818e4
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-02 10:19:18 +0500

    🐛 RefactoredViews

--- core/urls.py
+++ core/urls.py
@@ -22,10 +22,9 @@ from django.conf.urls.static import static
 # Using pre-build Auth Route
 from django.contrib.auth.views import LoginView,LogoutView
 from django.views.generic import TemplateView
-
+from users.views import register as RegisterationView
 # Custom Views
 from blog import views as blogViews
-from users import views as userView
 
 urlpatterns = [
     path('admin/' , admin.site.urls),
@@ -33,12 +32,11 @@ urlpatterns = [
 
     path('about/'    , TemplateView.as_view(template_name="html/about.html"), name='blog-about'),
 
-    path('register/' , userView.register , name='register'),
+    path('register/' , RegisterationView , name='user-register'),
     path('login/'    , LoginView.as_view(template_name  = 'html/login.html',redirect_authenticated_user=True), name='login'),
     path('logout/'   , LogoutView.as_view()   , name='logout'),
 
-    path('profile/'  , userView.profile  , name='profile'),
-    path('update_profile/', userView.update_profile  , name='update-profile'),
+    path('profile/'  , include('users.urls'), name='profile'),
 ] 
 
 # Only run this for development server
