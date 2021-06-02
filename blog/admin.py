b12baba7cf7d6c411703396beebe92be9f5b39fc
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-02 06:29:45 +0500

    📦 Added PostTags model for Posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,PostTag
 
-# Register Post model for admin dashboard.
+# Register PostTags Post model for admin dashboard.
+admin.site.register(PostTag)
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 from django.utils import timezone
 
+class PostTag(models.Model):
+    tag = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.tag
+    
 class Post(models.Model):
     title = models.CharField(max_length = 255)
     posted_datetime = models.DateTimeField(auto_now_add=True)
@@ -10,6 +16,8 @@ class Post(models.Model):
     content = models.TextField()
     
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # (Optional) A single post can have mutliple tags.
+    tags = models.ManyToManyField(PostTag,blank=True)
 
     def __str__(self):
         return self.title
