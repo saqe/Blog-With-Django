ff126bfd3e7bce3fce573dec2cabe638732964c8
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-02 06:12:30 +0500

    📦 Added PostModel relation with User

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register Post model for admin dashboard.
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    posted_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
