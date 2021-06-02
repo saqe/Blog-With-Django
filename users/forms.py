c6faa37aa0082502d1a48fca238c8a29ab6afadd
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-02 10:18:41 +0500

    👌 Fields to update first & last name of profile.

--- users/forms.py
+++ users/forms.py
@@ -18,7 +18,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username','email')
+        fields = ('username','first_name', 'last_name' ,'email')
 
 class ProfileUpdateForm(forms.ModelForm):
     country = CountryField()
