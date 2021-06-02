720428199f00a866f59fa7e2aa73d432201b00c5
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-02 10:21:44 +0500

    👌 IMPROVED View and Update user profile

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.my_profile, name='my-profile'),
+    path('update/', views.update_profile, name='update-profile'),
+    path('<int:pk>', views.ProfileDetailView.as_view(), name='view-profile'),
+]
