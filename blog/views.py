78eca7525e93d3e43967b0141a5d09197175ba36
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-14 18:33:03 +0500

    👌 IMPROVED code formating

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.contrib.admin.views.decorators import staff_member_required
+
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin)
@@ -13,19 +15,20 @@ from django.views.generic import (
 
 from .models import Post
 
-def home(request):
-    posts = Post\
-        .objects\
-        .filter(published=True)\
-        .order_by('posted_datetime')[:5]
+# # JUST Alternative way
+# def home_view(request):
+#     posts = Post\
+#         .objects\
+#         .filter(published=True)\
+#         .order_by('posted_datetime')[:5]
 
-    return render(
-        request, 
-        'html/home.html',
-        context={
-            'posts':posts,
-            'title':'HomePage'
-        })
+#     return render(
+#         request, 
+#         'html/home.html',
+#         context={
+#             'posts':posts,
+#             'title':'HomePage'
+#         })
 
 class PostListView(ListView):
     model = Post
@@ -33,39 +36,52 @@ class PostListView(ListView):
     paginate_by = 10
     context_object_name = 'posts'
     queryset = Post.objects.filter(published=True,featured=True)
-    
+
     def post(self,request):
         Post.object.create()
 
+# View post in detail
 class PostDetailView(DetailView):
     model = Post
     template_name='html/post.html'
     context_object_name = 'blogpost'
+    
 
-class PostCreateView(CreateView,LoginRequiredMixin):
+# Create a new post
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     template_name='html/form/create_post.html'
-    fields = ['title','content','tags']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-class PostUpdateView(UpdateView,LoginRequiredMixin,UserPassesTestMixin):
-    model = Post
-    template_name='html/post_form.html'
-    fields = ['title','content','tags']
     
+    fields = ['title','content','tags']
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    template_name='html/form/update_post.html'
+    fields = ['title','content','tags']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    # Verify if the user trying to do that is owner of that post
     def test_func(self):
         return self.request.user == self.get_object().author
 
-class PostDeleteView(DeleteView,LoginRequiredMixin,UserPassesTestMixin):
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Post
 
-    # Verify if the user trying to do that is 
+    # Verify if the user trying to do that is owner of that post
     def test_func(self):
-        return self.request.user == self.get_object().author
+        return 
+        (self.request.user == self.get_object().author)\
+        or\
+        self.request.user.is_superuser
+
+
+@staff_member_required
+def set_post_published(request):
+    pass
