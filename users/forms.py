77dec79010717d7a229b4563102eeb11735df78f
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-14 14:09:34 +0500

    👌 IMPROVED FirstName and Lastname fields UI.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext as _
+
 from django_countries.fields import CountryField
 from .models import Profile
 class UserRegisterForm(UserCreationForm):
@@ -24,7 +26,14 @@ class UpdateNameForm(forms.ModelForm):
         fields = ('first_name', 'last_name')
 
 class ProfileUpdateForm(forms.ModelForm):
+    # Profile Image
+    # profile_image = forms.ImageField(
+    #     label=_('Profile Image'),
+    #     required=False,
+    #     error_messages = {'invalid':_("Image files only")}, 
+    #     widget=forms.FileInput)
+
     country = CountryField()
     class Meta:
         model = Profile
-        fields = ('country','profile_image')
+        fields = ('gender','phone_number','country','state','city','zip_code')
