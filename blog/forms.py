68b32c659235bac5beaa5d4d556c845a8572ea1c
Saqib <18554574+saqe@users.noreply.github.com>
2021-06-26 12:03:47 +0500

    📦 NEW: Added Published & Featured Django Forms.

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+
+class FeaturedPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('featured',)
+
+class PublishedPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('published',)
