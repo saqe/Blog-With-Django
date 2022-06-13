08635f73b4f673e06cea10d47bdc2517692d4146
Saqib <18554574+saqe@users.noreply.github.com>
2022-06-14 00:44:37 +0500

    :sparkles: REFACTORED

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import (
@@ -17,65 +14,72 @@ from django.views.generic import (
 
 from .models import Post
 
+
 class PostListView(ListView):
     model = Post
-    template_name='blog/home.html'
+    template_name = 'blog/home.html'
     paginate_by = 10
     context_object_name = 'blog_posts'
-    queryset = Post.objects.filter(published=True,featured=True)
+    queryset = Post.objects.filter(published=True, featured=True)
 
 # View post in detail
+
+
 class PostDetailView(DetailView):
     model = Post
-    template_name='blog/post.html'
+    template_name = 'blog/post.html'
     context_object_name = 'blogpost'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["logged_in_user"] = self.request.user
         return context
-    
+
 
 # Create a new post
-class PostCreateView(LoginRequiredMixin,CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    template_name='blog/form/create_post.html'
-    
-    fields = ['title','content','tags']
+    template_name = 'blog/form/create_post.html'
+
+    fields = ['title', 'content', 'tags']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-        
-class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    template_name='blog/form/update_post.html'
-    fields = ['title','content','tags']
+    template_name = 'blog/form/update_post.html'
+    fields = ['title', 'content', 'tags']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     # Verify if the user trying to do that is owner of that post
     def test_func(self):
         return (self.request.user == self.get_object().author) or self.request.user.is_staff
 
-class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name='blog/form/delete_post.html'
+    template_name = 'blog/form/delete_post.html'
     success_url = reverse_lazy('my-profile')
     # Verify if the user trying to do that is owner of that post
+
     def test_func(self):
         return (self.request.user == self.get_object().author) or self.request.user.is_staff
 
 
-class PostApproveAdminUpdate(PermissionRequiredMixin,UpdateView):
+class PostApproveAdminUpdate(PermissionRequiredMixin, UpdateView):
     model = Post
     success_url = reverse_lazy('articles')
     permission_required = 'is_staff'
     fields = ['published']
 
-class PostFeaturedAdminUpdate(PermissionRequiredMixin,UpdateView):
+
+class PostFeaturedAdminUpdate(PermissionRequiredMixin, UpdateView):
     model = Post
     success_url = reverse_lazy('articles')
     permission_required = 'is_staff'
--- manager_dashboard/views.py
+++ manager_dashboard/views.py
@@ -6,43 +6,51 @@ from django.views.generic import ListView
 
 from blog.models import Post
 
-class DashboardView(PermissionRequiredMixin,TemplateView):
+
+class DashboardView(PermissionRequiredMixin, TemplateView):
     permission_required = 'is_staff'
     template_name = 'manager_dashboard/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["count_published_posts"] = Post.objects.filter(published=True).count()
-        context["count_featured_posts"] = Post.objects.filter(featured=True,published=True).count()
-        context["count_pending_posts"] = Post.objects.filter(published=False).count()
+        context["count_published_posts"] = Post.objects.filter(
+            published=True).count()
+        context["count_featured_posts"] = Post.objects.filter(
+            featured=True, published=True).count()
+        context["count_pending_posts"] = Post.objects.filter(
+            published=False).count()
         context["count_members"] = User.objects.count()
         return context
-    
-class MembersList(PermissionRequiredMixin,ListView):
+
+
+class MembersList(PermissionRequiredMixin, ListView):
     permission_required = 'is_staff'
     template_name = 'manager_dashboard/members.html'
     model = User
 
 
-class PostListView(PermissionRequiredMixin,ListView):
+class PostListView(PermissionRequiredMixin, ListView):
     permission_required = 'is_staff'
     template_name = 'manager_dashboard/articles-table.html'
     model = Post
 
-class PublishedPostListView(PermissionRequiredMixin,ListView):
+
+class PublishedPostListView(PermissionRequiredMixin, ListView):
     permission_required = 'is_staff'
     template_name = 'manager_dashboard/articles-table.html'
     model = Post
     queryset = Post.objects.filter(published=True)
 
-class DraftPostListView(PermissionRequiredMixin,ListView):
+
+class DraftPostListView(PermissionRequiredMixin, ListView):
     permission_required = 'is_staff'
     template_name = 'manager_dashboard/articles-table.html'
     model = Post
     queryset = Post.objects.filter(published=False)
 
-class FeaturedPostListView(PermissionRequiredMixin,ListView):
+
+class FeaturedPostListView(PermissionRequiredMixin, ListView):
     permission_required = 'is_staff'
     template_name = 'manager_dashboard/articles-table.html'
     model = Post
-    queryset = Post.objects.filter(published=True,featured=True)
+    queryset = Post.objects.filter(published=True, featured=True)
